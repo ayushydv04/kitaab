c494f8440be7dd500b01820e7b956586172fccb6
Ayush Kumar Yadav <113637305+ayushydv04@users.noreply.github.com>
2024-11-14 14:45:13 +0530

    first commit

--- Frontend/src/api/PostApi.jsx
+++ Frontend/src/api/PostApi.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 
 const api = axios.create({
     baseURL: "http://localhost:5000",
+    // mongodb+srv://new-ayush_04:<db_password>@kitaabproducts.x6f1s.mongodb.net/
     
 });
 
--- Frontend/src/pages/Orders.jsx
+++ Frontend/src/pages/Orders.jsx
@@ -37,8 +37,8 @@ const Orders = () => {
                 <div className='flex items-center gap-2'>
                   <p className='min-w-2 h-2 rounded-full bg-green-500'></p>
                   <p className='text-sm md:text-base'>Ready to ship</p>
-
                 </div>
+                <button className='border px-4 py-2 text-sm font-medium rounded-sm'>Track Order</button>
 
               </div>
 
